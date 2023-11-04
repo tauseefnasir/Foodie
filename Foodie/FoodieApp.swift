feb0b11a882d08abb2bd96e86592099dc030a251
tauseefnasir <tauseefnasir501@gmail.com>
2023-11-04 14:09:07 +0500

    Initial Commit

---
+++ Foodie/FoodieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieApp.swift
+//  Foodie
+//
+//  Created by Nasir on 04/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
